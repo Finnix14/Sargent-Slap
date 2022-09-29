8ba6584b2418994c58f3477639be7db7959a0076
Finnix14 <49366542+Finnix14@users.noreply.github.com>
2022-09-29 19:20:18 +0100

    Mouse Drag and New UI

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -7,8 +7,11 @@ public class EnemyManager : MonoBehaviour
 {
     public Transform[] m_SpawnPoints;
     public Transform[] m_BombSpawn;
+    public Transform[] m_TrampolineSpawn;
+
     public GameObject m_BombPrefab;
     public GameObject m_EnemyPrefabs;
+    public GameObject m_TrampolinePrefab;
     void Start()
     {
 
@@ -27,8 +30,15 @@ public class EnemyManager : MonoBehaviour
 
     public void SpawnNewBomb()
     {
-        int randonNumber = Mathf.RoundToInt(Random.Range(0f, m_SpawnPoints.Length - 1));
+        int randonNumber = Mathf.RoundToInt(Random.Range(0f, m_BombSpawn.Length - 1));
 
         Instantiate(m_BombPrefab, m_BombSpawn[0].transform.position, Quaternion.identity);
+    }
+
+    public void SpawnNewTrampoline()
+    {
+        int randonNumber = Mathf.RoundToInt(Random.Range(0f, m_TrampolineSpawn.Length - 1));
+
+        Instantiate(m_TrampolinePrefab, m_TrampolineSpawn[0].transform.position, Quaternion.identity);
     }
 }
